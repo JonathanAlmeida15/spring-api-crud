1e2b3aaf75062f81d7415e7b0d066f8960b8f7ee
JonathanAlmeida15 <jonathangabrielcar@gmail.com>
2025-11-18 03:07:56 -0300

    Subindo atualização que adiciona botões de Excluir e Editar, na tela de lista de clientes, botões funcionais, resta melhorar a formatação dos labels na pagina de edição de clientes

--- django_dashboard/dashboard/urls.py
+++ django_dashboard/dashboard/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, ClientListView, ClientCreateView
+from .views import home, ClientListView, ClientCreateView, ClientUpdateView, ClientDeleteView
 
 app_name = 'dashboard'
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', home, name='home'),
     path('clients/', ClientListView.as_view(), name='client_list'),
     path('clients/new/', ClientCreateView.as_view(), name='client_create'),
+    path('clients/<int:pk>/edit/', ClientUpdateView.as_view(), name='client_edit'),
+    path('clients/<int:pk>/delete/', ClientDeleteView.as_view(), name='client_delete'),
 ]
--- django_dashboard/dashboard/views.py
+++ django_dashboard/dashboard/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, CreateView
 from django.urls import reverse_lazy
 from clients.models import Client   # <-- SOMENTE ESTE IMPORT
+from django.views.generic import UpdateView, DeleteView
 
 def home(request):
     return render(request, 'dashboard/home.html')
@@ -16,3 +17,14 @@ class ClientCreateView(CreateView):
     fields = ['first_name', 'last_name', 'email', 'phone', 'company', 'notes']
     template_name = "dashboard/client_form.html"
     success_url = reverse_lazy('dashboard:client_list')
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    fields = ['first_name', 'last_name', 'email', 'phone', 'company', 'notes']
+    template_name = "clients/client_form.html"
+    success_url = reverse_lazy('dashboard:client_list')
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    template_name = "clients/client_confirm_delete.html"
+    success_url = reverse_lazy("dashboard:client_list")
