69e1dff355fd0fbfe2daa5fb780760dcb6345b3d
JonathanAlmeida15 <jonathangabrielcar@gmail.com>
2025-11-22 04:52:09 -0300

    Excluindo uma pasta errada do diretorio, no próximo push, preciso resolver o problema do package e também testar os códigos

--- django_dashboard/clients/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Client
-
-@admin.register(Client)
-class ClientAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'email', 'company', 'created_at')
-    search_fields = ('first_name', 'last_name', 'email', 'company')
-    list_filter = ('created_at',)
